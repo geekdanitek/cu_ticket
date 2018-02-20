0ecefd6e1bc9ac39e50a6702b42286ef7f53d71c
geekdanitek <adedejiayokunledaniel@gmail.com>
2018-02-20 15:47:45 +0100

    nav-fix

--- resources/views/layouts/admin_master.blade.php
+++ resources/views/layouts/admin_master.blade.php
@@ -5,32 +5,40 @@
 		<nav class="navbar navbar-default">
 			<div class="container">
 				<div class="navbar-header">
+					 <button type="button" class="navbar-toggle nav-btn-click" data-toggle="collapse" data-target="#myNavbar">
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span> 
+      				 </button>
 					<a class="navbar-brand" href="#">
         				<img alt="Brand" src="{{ url('images/CU_LOGO_BRAND.jpg') }}" class="img-responsive">		
       				</a>
-      					<span class="brand-text">CU Ticket System</span>
+      					<span class="brand-text ">CU Ticket System</span>
       
 				</div>
-				<div class="right-nav">
+				<div class="collapse navbar-collapse" id="myNavbar">
+					<div class="right-nav">
+
 						<ul class="nav navbar-nav navbar-right">
+									
+							<li><a href="#"><span class="glyphicon glyphicon-user right-nav"></span>&nbsp; Welcome, @yield('name')</a></li>
+							@if(session()->has('user') == true)
+							<a class="btn btn-primary btn-xs navbar-btn logout-btn" href="{{route('logout')}}">
 								
-					<li><a href="#"><span class="glyphicon glyphicon-user right-nav"></span>&nbsp; Welcome, @yield('name')</a></li>
-						@if(session()->has('user') == true)
-						<a class="btn btn-primary btn-xs navbar-btn logout-btn" href="{{route('logout')}}">
+									<span class="glyphicon glyphicon-log-out"></span> Logout
 							
-								<span class="glyphicon glyphicon-log-out"></span> Logout
-						
-						</a>
-						@endif
-						@if(session()->has('admin_user') == true)
-						<a class="btn btn-primary btn-xs navbar-btn logout-btn" href="{{route('logout')}}">
+							</a>
+							@endif
+							@if(session()->has('admin_user') == true)
+							<a class="btn btn-primary btn-xs navbar-btn logout-btn" href="{{route('logout')}}">
+								
+									<span class="glyphicon glyphicon-log-out"></span> Logout
 							
-								<span class="glyphicon glyphicon-log-out"></span> Logout
-						
-						</a>
-						@endif
+							</a>
+							@endif
 						</ul>
-						
-			</div>
+							
+					</div>
+				</div>
 		</nav>
 @endsection
